be66d896c9933fb45f0543e33c8c1e36ae1e6b37
hotbk <tuanvietbk45@gmail.com>
2026-05-14 09:18:10 +0000

    test1

---
+++ run_env/dags/test_mssql_connection.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
+from datetime import datetime
+
+def test_mssql():
+    hook = MsSqlHook(mssql_conn_id="mssql_vietdb")
+    result = hook.get_records("SELECT @@version")
+    print("✓ Connection success!")
+    print(result)
+
+with DAG(
+    dag_id="test_mssql_connection",
+    start_date=datetime(2026, 5, 1),
+    schedule_interval=None,
+    catchup=False
+) as dag:
+    test = PythonOperator(task_id="test", python_callable=test_mssql)
